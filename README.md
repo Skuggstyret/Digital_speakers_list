a5a246b83fd254f2a20b775daca15b805ec223f7
ErikFrennborn <erikfre@student.chalmers.se>
2021-07-08 22:45:48 +0200

    Added simple trustbased remote addation

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 ## Mål
 Underlätta möten genom att ta bort den fysika talare listan.
 
+Medlamer kan nu skicka sitt kort kommando till server och läggas till på talar listan. Detta bygger helt på tillit, det finns ingen form av säkerhelt. Vem som helst kan skicka kan skicka vad som helst och system kommer ta det som input.
+
 ## Förbättring potinial
 Skriva om i bättre språk, *host* Rust *host* va. Samt skriva extantion för att låta medlem skriva upp sig själv på talar listan, exempel vis genom en hemsida eller command line tool.
--- prototyp/main.py
+++ prototyp/main.py
@@ -1,67 +1,112 @@
-import os, toml, sys
+import os, toml, sys, socket, argparse, threading, time
 
+# TODO: Kanske borde flyttas till config file, men känns onödigt
+# i nuläget
+HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
+PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
+
+# Global variable
 speaker_list = []
 speakers = {}
+terminal_lock = threading.Semaphore()
+speaker_lock = threading.Semaphore()
+
 
 clear = lambda: os.system('clear')
 
 def addSpeaker():
-    while True:
-        tag = input("Välj kort kommando för medlemen:")
-        if tag in speakers:
-            print("Tag används redan")
-        else:
-            break
-    name = input("Namn på medlemen: ")
-    speakers[tag] = [name,0]
+    with terminal_lock:
+        while True:
+            tag = input("Välj kort kommando för medlemen:")
+            if tag in speakers:
+                print("Tag används redan")
+            else:
+                break
+        name = input("Namn på medlemen: ")
+        speakers[tag] = [name,0]
 
 def speakerFromFile(path):
     file_data = toml.load(path)
-    for speaker in file_data["talar"]:
-        speakers[speaker[0]] = [speaker[1],0]
+    with speaker_lock:
+        for speaker in file_data["talar"]:
+            speakers[speaker[0]] = [speaker[1],0]
 
-def printSpeakerList():
-    for speaker in speaker_list:
-        print(speakers[speaker][0])
 
-def addSpeakerToList(tag):
+def addSpeakerToList(tag, remote=False):
     if tag in speakers:
-        speaker_list.append(tag)
-        speaker_list.sort(key=lambda x: speakers[x][1])
+        with speaker_lock:
+            speaker_list.append(tag)
+            speaker_list.sort(key=lambda x: speakers[x][1])
+            return True
     else:
-        print("Ingen sådan talare")
+        if not remote:
+            with terminal_lock:
+                print("Ingen sådan talare")
+        return False
 
 def nextSpeaker():
-    global speaker_list
-    speaker = speakers[speaker_list[0]]
-    speaker[1] += 1
-    speaker_list = speaker_list[1:]
+    with speaker_lock:
+        global speaker_list
+        speaker = speakers[speaker_list[0]]
+        speaker[1] += 1
+        speaker_list = speaker_list[1:]
 
 def resetCount():
-    if len(speaker_list) != 0:
-        while True:
-            sure = input("Talar listan är inte tom är du säker?(y/n)")
-            if sure == "n":
-                return
-            elif sure == "y":
-                break
+    with terminal_lock:
+        if len(speaker_list) != 0:
+            while True:
+                sure = input("Talar listan är inte tom är du säker?(y/n)")
+                if sure == "n":
+                    return
+                elif sure == "y":
+                    break
 
-    for speaker in speakers:
-        speakers[speaker][1] = 0
+        with speaker_lock:
+            for speaker in speakers:
+                speakers[speaker][1] = 0
 
-def main(argv):
-    if len(argv) != 0:
-        speakerFromFile(argv[0])
-    while True:
+def udpListner():
+    # Letar efter för char i UDP medelande och lägger till i
+    # talar listan.
+
+    UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+
+    UDPServerSocket.bind((HOST, PORT))
+
+    t = threading.currentThread()
+    while getattr(t, "do_run", True):
+        # TODO: Fixa v, block till den från input och
+        # stänger därför inte ner som den ska.
+        bytesAddressPair = UDPServerSocket.recvfrom(1024)
+
+        message = bytesAddressPair[0]
+        address = bytesAddressPair[1]
+
+        tag = chr(message[0])
+
+        if (addSpeakerToList(tag,True)):
+            render()
+
+
+def render():
+    with terminal_lock:
         clear()
         print("Skuggstyret talarlista")
         print("----------------------")
 
-        printSpeakerList()
+        for speaker in speaker_list:
+            print(speakers[speaker][0])
 
+def main(argv):
+    udp_lister =  threading.Thread(target=udpListner, daemon=True)
+    udp_lister.start()
+    if len(argv) != 0:
+        speakerFromFile(argv[0])
+    while True:
+        render()
         command = input()
-
         if command == ":q":
+            udp_lister.do_run = False
             exit()
         elif command == ":a":
             addSpeaker()

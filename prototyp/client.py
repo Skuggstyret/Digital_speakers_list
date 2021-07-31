5c52c01b1d444aa9b8a160ea42eb2b5941d53511
ErikFrennborn <erikfre@student.chalmers.se>
2021-07-31 09:06:43 +0200

    Made client more flexiable

--- prototyp/client.py
+++ prototyp/client.py
@@ -1,9 +1,14 @@
-import socket, sys
+import socket, sys, os
 
 if len(sys.argv) < 2:
-    print("Ange ip och port som argument <ip> <port>")
+    ip = input("Ip: ")
+    port = int(input("Port: "))
+else:
+    ip = sys.argv[1]
+    port = int(sys.argv[2])
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
 while True:
+    os.system('clear')
     message = input("Short command: ")
-    sock.sendto(bytes(message,"utf-8"), (sys.argv[1], int(sys.argv[2])))
+    sock.sendto(bytes(message,"utf-8"), (ip, port))

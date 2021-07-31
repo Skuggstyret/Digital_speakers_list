9aea4169a5303e0827721924cb015ad53ca4ff5d
ErikFrennborn <erikfre@student.chalmers.se>
2021-07-31 19:26:13 +0200

    Implemented default command for client

--- prototyp/client.py
+++ prototyp/client.py
@@ -1,6 +1,8 @@
 import socket, sys, os
 
-if len(sys.argv) < 2:
+default_command = ""
+
+if len(sys.argv) < 3:
     ip = input("Ip: ")
     port = int(input("Port: "))
 else:
@@ -11,4 +13,12 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
 while True:
     os.system('clear')
     message = input("Short command: ")
+    if message == "":
+        message = default_command
+    elif message.split()[0] == ":d":
+        if len(message.split()) > 1:
+            default_command = message.split()[1]
+    elif message == ":h":
+        input("HELP SCREEN TODO")
+
     sock.sendto(bytes(message,"utf-8"), (ip, port))

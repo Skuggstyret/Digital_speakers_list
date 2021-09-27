61220cd26558518d898deb3a429c24b71b5ca9e4
ErikFrennborn <erikfre@student.chalmers.se>
2021-09-27 22:45:07 +0200

    Swap to double speakers list

--- prototyp/server.py
+++ prototyp/server.py
@@ -2,14 +2,14 @@ import os, toml, sys,threading, datetime
 from flask import Flask, jsonify
 from flask_api import status
 import json
+from functools import reduce
 
 app = Flask(__name__)
 
 # Global variable
-speaker_list = []
+speaker_lists = [[],[]]
 speakers = {}
 speaker_lock = threading.Semaphore()
-
 
 # TODO: Byt till två talarlistor
 
@@ -23,46 +23,55 @@ def getSpeakers():
 def addSpeaker(name):
     speakers[name] = [0,0]
 
-
-
 # list
 @app.route("/list/add/<name>", methods=['POST'])
 def addSpeakerToList(name):
     if name in speakers:
         with speaker_lock:
-            if name in speaker_list:
-                return '', status.HTTP_400_BAD_REQUEST
-            speaker_list.append(name)
-            speaker_list.sort(key=lambda x: speakers[x][1])
-            return '',status.HTTP_202_ACCEPTED
+            if name in speaker_lists[0] or name in speaker_lists[1]:
+                return 'Speaker already on list', status.HTTP_400_BAD_REQUEST
+            index = 0
+            if speakers[name][0] > 0:
+                temp = (reduce(lambda x,y: [x[0]+y[0], x[1]+y[1]], map(lambda x:(speakers[x][0],1) ,speakers), [0,0]))
+                index = int(speakers[name][0] > (temp[0]/temp[1])/2)
+            speaker_lists[index].append(name)
+            return 'Speaker added',status.HTTP_202_ACCEPTED
     else:
-        return '', status.HTTP_400_BAD_REQUEST
+        return 'No such speaker', status.HTTP_400_BAD_REQUEST
 
 @app.route("/list/remove/<name>",methods=['POST'])
 def removeSpeakerFromList(name):
     if name in speakers:
         with speaker_lock:
-            if name not in speaker_list:
-                return '', status.HTTP_400_BAD_REQUEST
-            speaker_list.remove(name)
-            return '', status.HTTP_200_OK
+            index = 0
+            if name not in speaker_lists[0]:
+                index = 1
+                if name not in speaker_lists[1]:
+                    return 'No such speaker on list', status.HTTP_400_BAD_REQUEST
+            speaker_lists[index].remove(name)
+            return 'Speaker removed', status.HTTP_200_OK
     else:
-        return '', status.HTTP_400_BAD_REQUEST
+        return 'No such speaker', status.HTTP_400_BAD_REQUEST
 
 @app.route("/list/get",methods=['GET'])
 def getSpeakersOnList():
-    return jsonify(speaker_list), status.HTTP_200_OK
+    return jsonify(data=speaker_lists), status.HTTP_200_OK
 
 
 @app.route("/list/next",methods=['POST'])
 def nextSpeaker():
     with speaker_lock:
         global speaker_list
-        next_speaker = speaker_list[0]
+        index = 0
+        if len(speaker_lists[0]) == 0:
+            index = 1
+        if len(speaker_lists[index]) == 0 :
+            return 'Speakers list is empty', status.HTTP_410_GONE
+        next_speaker = speaker_lists[index][0]
         speaker = speakers[next_speaker]
         speaker[0] += 1
         speaker[1] += 1
-        speaker_list = speaker_list[1:]
+        speaker_lists[index] = speaker_lists[index][1:]
         return next_speaker, status.HTTP_202_ACCEPTED
 
 @app.route("/list/reset", methods=['POST'])
@@ -92,6 +101,6 @@ if __name__ == "__main__":
     if len(sys.argv) > 1:
         file_data = toml.load(sys.argv[1])
         with speaker_lock:
-            for speaker in file_data["talar"]:
+            for speaker in file_data["speakers"]:
                 speakers[speaker] = [0,0]
     app.run(debug=True, use_reloader=False)

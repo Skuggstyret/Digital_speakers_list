702239bfb5eb1fc5f55fc9b2082f1a097698ec33
Lucas Glimfjord <lucas@glimfjord.com>
2021-10-05 00:02:17 +0200

    Creates separate component for the nested speaker list

---
+++ prototyp/frontend/src/components/Nested_list.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-card class="pa-2" elevation="6">
+    <v-card-title>Speakers List</v-card-title>
+    <NestedList v-if="list_depth < items.length - 1" :list_depth="list_depth + 1" :items="items"/>
+    <v-list>
+      <v-list-item v-for="item in items[list_depth]" :key="item.number"> {{ item.number }}: {{ item.name }} </v-list-item>
+    </v-list>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "NestedList",
+  props: ["items", "list_depth"]
+
+}
+</script>
+
